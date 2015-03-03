075f6343929cec512a62ce63db26af5d51150f09
Jérôme Nadaud <jerome.nadaud@prestashop.com>
2015-03-03 12:02:01 +0100

    // Version updated to v0.7.4

--- cashondelivery.php
+++ cashondelivery.php
@@ -33,7 +33,7 @@ class CashOnDelivery extends PaymentModule
 	{
 		$this->name = 'cashondelivery';
 		$this->tab = 'payments_gateways';
-		$this->version = '0.7.3';
+		$this->version = '0.7.4';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 1;
 		$this->controllers = array('validation');
